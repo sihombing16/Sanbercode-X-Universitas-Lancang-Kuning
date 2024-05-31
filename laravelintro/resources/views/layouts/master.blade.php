606b723d8f8af8d2b6a1cdc0fa96d24ef270aade
username anda <email anda>
2024-05-31 07:53:51 +0700

    Tugas 13:laravel migration

--- laravelintro/resources/views/layouts/master.blade.php
+++ laravelintro/resources/views/layouts/master.blade.php
@@ -24,20 +24,15 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
-      <img src="{{asset('/admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{asset('/admin/dist/img/AdminLTELogo.png')}}"alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
     <!-- Sidebar -->
     @include('partials.sidebar')
-    <!-- /.sidebar user (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-          <img src="{{asset('/admin/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2 alt="User image"
-      <div>
-      <div class="info">
-      <a href="#" class="d-block">Alexander Pierce</a>
-      <div>
-    </div>
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      
+      </div>
   </aside>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
